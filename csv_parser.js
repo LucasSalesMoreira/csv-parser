1d9ef60cda0d471c90e5e12d13dafe8d04fdaf4f
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2024-10-30 22:30:09 -0300

    update
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- csv_parser.js
+++ csv_parser.js
@@ -1,8 +1,5 @@
 const fs = require('node:fs/promises');
 
-const originFileType = '.csv';
-const originFileName = 'CADPROD';
-
 function getColumnsArray(data) {
   return data
     .split('\n')[0]
@@ -20,33 +17,77 @@ function getColumnsOfLineArray(line) {
   return line.replaceAll('"', '').split(';').map(c => c.trim());
 }
 
+async function extractProduct(originFileName, originFileType) {
+  const data = await fs.readFile(`${originFileName}${originFileType}`, { encoding: 'utf8' });
+  const columns = getColumnsArray(data);
+  const lines = getLinesArray(data);
+  const objectArray = [];
+
+  for (let l = 0; l < lines.length; l++) {
+    let columnsOfLineArray = getColumnsOfLineArray(lines[l]);
+    let codProdField = columnsOfLineArray[columns.indexOf('cod_prod')];
+    let codFabField = columnsOfLineArray[columns.indexOf('cod_fab')];
+    let codBarraField = columnsOfLineArray[columns.indexOf('cod_barra')];
+    let desProdField = columnsOfLineArray[columns.indexOf('des_prod')];
+    let valCompraField = columnsOfLineArray[columns.indexOf('val_compra')];
+    let valCustoField = columnsOfLineArray[columns.indexOf('val_custo')];
+    let precoField = columnsOfLineArray[columns.indexOf('preco1')];
+    objectArray.push({
+      codProd: codProdField != undefined ? codProdField : null,
+      codFab: codFabField != undefined ? codFabField : null,
+      desProd: desProdField != undefined ? desProdField : null,
+      valCompra: valCompraField != undefined ? valCompraField : null,
+      valCusto: valCustoField != undefined ? valCustoField : null,
+      preco: precoField != undefined ? precoField : null
+    });
+  }
+  await fs.writeFile(`${originFileName}.json`, JSON.stringify(objectArray));
+}
+
+async function extractSupplier(originFileName, originFileType) {
+  const data = await fs.readFile(`${originFileName}${originFileType}`, { encoding: 'utf8' });
+  const columns = getColumnsArray(data);
+  const lines = getLinesArray(data);
+  const objectArray = [];
+
+  for (let l = 0; l < lines.length; l++) {
+    let columnsOfLineArray = getColumnsOfLineArray(lines[l]);
+    let razsocField = columnsOfLineArray[columns.indexOf('razsoc')];
+    let fantasiaField = columnsOfLineArray[columns.indexOf('fantasia')];
+    let endforField = columnsOfLineArray[columns.indexOf('endfor')];
+    let telfor1Field = columnsOfLineArray[columns.indexOf('telfor1')];
+    let telfor2Field = columnsOfLineArray[columns.indexOf('telfor2')];
+    let faxforField = columnsOfLineArray[columns.indexOf('faxfor')];
+    let baiforField = columnsOfLineArray[columns.indexOf('baifor')];
+    let cidforField = columnsOfLineArray[columns.indexOf('cidfor')];
+    let estforField = columnsOfLineArray[columns.indexOf('estfor')];
+    let cepforField = columnsOfLineArray[columns.indexOf('cepfor')];
+    let cgcforField = columnsOfLineArray[columns.indexOf('cgcfor')];
+    let ieforField = columnsOfLineArray[columns.indexOf('iefor')];
+    let represField = columnsOfLineArray[columns.indexOf('repres')];
+    
+    objectArray.push({
+      corporateReason: razsocField != undefined ? razsocField : null,
+      name: fantasiaField != undefined ? fantasiaField : null,
+      address: endforField != undefined ? endforField : null,
+      phone: telfor1Field != undefined ? telfor1Field : null,
+      district: baiforField != undefined ? baiforField : null,
+      city: cidforField != undefined ? cidforField : null,
+      state: estforField != undefined ? estforField : null,
+      cep: cepforField != undefined ? cepforField : null,
+      cgcfor: cgcforField != undefined ? cgcforField : null,
+      iefor: ieforField != undefined ? ieforField : null,
+      repres: represField != undefined ? represField : null
+    });
+  }
+  await fs.writeFile(`${originFileName}.json`, JSON.stringify(objectArray));
+}
+
 async function run() {
   try {
-    const data = await fs.readFile(`${originFileName}${originFileType}`, { encoding: 'utf8' });
-    const columns = getColumnsArray(data);
-    const lines = getLinesArray(data);
-
-    const objectArray = [];
-
-    for (let l = 0; l < lines.length; l++) {
-      let columnsOfLineArray = getColumnsOfLineArray(lines[l]);
-      let codProdField = columnsOfLineArray[columns.indexOf('cod_prod')];
-      let codFabField = columnsOfLineArray[columns.indexOf('cod_fab')];
-      let codBarraField = columnsOfLineArray[columns.indexOf('cod_barra')];
-      let desProdField = columnsOfLineArray[columns.indexOf('des_prod')];
-      let valCompraField = columnsOfLineArray[columns.indexOf('val_compra')];
-      let valCustoField = columnsOfLineArray[columns.indexOf('val_custo')];
-      let precoField = columnsOfLineArray[columns.indexOf('preco1')];
-      objectArray.push({
-        codProd: codProdField != undefined ? codProdField : null,
-        codFab: codFabField != undefined ? codFabField : null,
-        desProd: desProdField != undefined ? desProdField : null,
-        valCompra: valCompraField != undefined ? valCompraField : null,
-        valCusto: valCustoField != undefined ? valCustoField : null,
-        preco: precoField != undefined ? precoField : null
-      });
-    }
-    await fs.writeFile(`${originFileName}.json`, JSON.stringify(objectArray));
+    const originFileType = '.csv';
+    await extractProduct('CADPROD', originFileType);
+    await extractSupplier('', originFileType);
   } catch (err) {
     console.log(err);
   }
